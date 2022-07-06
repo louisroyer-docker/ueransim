e7dfb03bf08906faba257499618ce03e92415305
Louis Royer <infos.louis.royer@gmail.com>
2022-07-06 17:47:16 +0200

    routing

--- ue/routing.sh
+++ ue/routing.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 tun_is_up () {
-	ip link ls | grep "state UP" | grep uesimtun 2>&1> /dev/null
+	ip link ls | grep "LOWER_UP" | grep uesimtun 1>&2> /dev/null
 	return $?
 }
 
@@ -16,7 +16,7 @@ done
 # use something else than "default" route.
 # The only advantage of not hardcoding uesimtun0 is the case where you have multiple
 # uesimtun interfaces, but only 1 is up. This will probably not happend.
-T=$(ip link ls | grep "state UP" | grep uesimtun | awk 'BEGIN {FS=": "}; {print $1; exit}')
+T=$(ip link ls | grep "LOWER_UP" | grep uesimtun | awk 'BEGIN {FS=": "}; {print $1; exit}')
 # By default, UERANSIM only create route on a specific table to preserve default route.
 # This is fine in a VM, where you might want to keep the route and use nr-binder.
 # But it is better to have a container with the same behaviour as a real UE.
